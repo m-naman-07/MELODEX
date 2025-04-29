634b0780ff3cd987f4afa327a55e4926244c616a
Naman Mishra <naman@Namans-MacBook-Air.local>
2025-04-30 00:32:23 +0530

    remove folder

--- backEnd/src/routes/image.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import getImageByTitle from "../controllers/image.controller.js";
-
-const router = Router();
-
-router.route("/getImageByTitle").get(getImageByTitle);
-
-export default router;
